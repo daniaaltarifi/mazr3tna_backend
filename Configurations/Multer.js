7bef6959883b83a13a3d932efe90defb1940f5f6
loransalkhateeb <lwrnsalkhtyb9@gmail.com>
2024-12-07 09:30:27 +0300

    Update Create Cloudinary of this project

---
+++ Configurations/Multer.js
@@ -0,0 +1,15 @@
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const multer = require('multer');
+const cloudinary = require('../Configurations/cloudinaryConfig');
+
+const storage = new CloudinaryStorage({
+    cloudinary: cloudinary,
+    params: {
+        folder: 'uploads/chalets_images', 
+        allowed_formats: ['jpg', 'png', 'mp4'], 
+    },
+});
+
+const upload = multer({ storage });
+
+module.exports = upload;
--- Router/ProductRouter.js
+++ Router/ProductRouter.js
@@ -1,45 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const ProductController = require("../Controller/ProductController.js");
-const multer = require("multer");
-const path = require("path");
-const fs = require("fs");
-
-// Configure multer storage
-const customStorage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "images"); // Destination folder
-  },
-  filename: (req, file, cb) => {
-    const ext = path.extname(file.originalname);
-    const baseName = path.basename(file.originalname, ext);
-    const filePath = path.join("images", file.originalname);
-
-    // Check if file already exists with the same name
-    if (fs.existsSync(filePath)) {
-      // If the file exists, don't upload again
-      cb(null, file.originalname);  // Same filename will not be added again
-    } else {
-      // If file doesn't exist, proceed with saving the file
-      cb(null, file.originalname);
-    }
-  },
-});
-
-// Middleware to handle file upload
-const upload = multer({
-  storage: customStorage,
-  fileFilter: (req, file, cb) => {
-    // Optionally, you can filter file types if needed
-    cb(null, true);
-  },
-});
-
+const multer = require('../Configurations/Multer');
 
 // Product routes
-router.post("/add", upload.array("img", 5), ProductController.addProduct);
+router.post("/add", multer.array("img", 5), ProductController.addProduct);
 router.delete("/delete/:id", ProductController.deleteProduct);
-router.put("/update/:id", upload.array("img", 5), ProductController.updateProduct);
+router.put("/update/:id", multer.array("img", 5), ProductController.updateProduct);
 router.get("/getbyid/:id",ProductController.getProductDetails)
 router.get("/getbyidcms/:id",ProductController.getProductByIdCms)
 router.get("/get/allproducts", ProductController.getAllProducts);
